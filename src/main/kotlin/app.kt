ebace4017f36f156b26058fd0cfc05e837d54634
Peter Kurlak <pkurlak@gmail.com>
2017-12-29 07:07:19 -0600

    Draw a red OpenGL window

--- src/main/kotlin/app.kt
+++ src/main/kotlin/app.kt
@@ -1,5 +1,6 @@
 package com.petesburgh.shooter
 
 fun main(args: Array<String>) {
+    println("Hello, world")
     Game().run()
 }
--- src/main/kotlin/game.kt
+++ src/main/kotlin/game.kt
@@ -1,18 +1,76 @@
 package com.petesburgh.shooter
 
-import org.lwjgl.glfw.GLFW.glfwInit
+import org.lwjgl.glfw.Callbacks.glfwFreeCallbacks
+import org.lwjgl.glfw.GLFW.*
 import org.lwjgl.glfw.GLFWErrorCallback
+import org.lwjgl.opengl.GL
+import org.lwjgl.opengl.GL11.*
+import org.lwjgl.system.MemoryStack
+import org.lwjgl.system.MemoryUtil.NULL
 
 class Game {
+
     fun run() {
-        init()
+        val window  = init()
+        loop(window)
+
+        glfwFreeCallbacks(window)
+        glfwDestroyWindow(window)
+
+        glfwTerminate()
+        glfwSetErrorCallback(null).free()
     }
 
-    private fun init() {
+    private fun init(): Long {
         GLFWErrorCallback.createPrint(System.err).set()
 
         if (!glfwInit()) {
+            throw IllegalStateException("Unable to initialize GLFW")
+        }
 
+        glfwDefaultWindowHints()
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE)
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE)
+
+        val window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL)
+        if (window == NULL) {
+            throw RuntimeException("Failed to create the GLFW window")
+        }
+
+        glfwSetKeyCallback(window, fun(window, key, scancode, action, mods) {
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+                glfwSetWindowShouldClose(window, true)
+            }
+        })
+
+        MemoryStack.stackPush().use {
+            val pWidth = it.mallocInt(1)
+            val pHeight = it.mallocInt(1)
+
+            glfwGetWindowSize(window, pWidth, pHeight)
+
+            val vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor())
+            glfwSetWindowPos(
+                    window,
+                    (vidMode.width() - pWidth.get(0)) / 2,
+                    (vidMode.height() - pHeight.get(0)) / 2)
+        }
+
+        glfwMakeContextCurrent(window)
+        glfwSwapInterval(1) // enable v-sync
+        glfwShowWindow(window)
+        return window
+    }
+
+    private fun loop(window: Long) {
+        GL.createCapabilities()
+
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f)
+
+        while (!glfwWindowShouldClose(window)) {
+            glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
+            glfwSwapBuffers(window)
+            glfwPollEvents()
         }
     }
 }
